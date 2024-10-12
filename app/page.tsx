b5f6db0b2080b262ed509ff458658087792bdbab
LT <thipelebo00@gmail.com>
2024-10-12 20:12:36 +0200

    sign in redirect

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,10 @@ import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  const user = currentUser();
 
-  if (!user) {
-    redirect("/sign-in");
-  } else {
-    redirect("/dashboard");
-  }
+  redirect("/sign-in");
+
+
 
   return null;
 }
